1bd9f396b319aecc46386d3036202fe39d2dff64
sanghamitrapadhi <43683358+sanghamitrapadhi@users.noreply.github.com>
2018-10-01 17:00:20 +0530

    Update TransactionService.java

--- src/main/java/com/wallet/service/TransactionService.java
+++ src/main/java/com/wallet/service/TransactionService.java
@@ -2,11 +2,15 @@ package com.wallet.service;
 
 import org.springframework.stereotype.Service;
 
+import com.wallet.constant.TransactionType;
+import com.wallet.message.ResponseMessage;
 import com.wallet.model.Transaction;
 
 @Service
 public interface TransactionService {
 	
 	public void save(Transaction transaction);
+
+	public ResponseMessage saveTransaction(TransactionType type, String playerCode, Double amount, ResponseMessage response);
 
 }
